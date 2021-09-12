42ebe91524dd51a8b2dc10e2b31837f77f51b167
jehmes <jeihmesthales@hotmail.com>
2021-09-12 17:05:43 -0300

    home quase pronta

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -12,4 +12,11 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  goToTexts() {
+    if (document.body.scrollTop === 0) {
+      document.body.scrollTop = 500;
+      document.documentElement.scrollTop = 500;
+    }
+  
+  }
 }
