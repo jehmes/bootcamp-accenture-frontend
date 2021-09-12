ab7d5e2bcbb19932c46d2e9bc432b8c08c140525
jehmes <jeihmesthales@hotmail.com>
2021-09-11 23:31:17 -0300

    CSS do header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
+import { MatSliderModule } from '@angular/material/slider';
+import { FooterComponent } from './components/template/footer/footer.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    FormsModule
+    FormsModule,
+    MatSliderModule,
+    MatToolbarModule,
+    MatButtonModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
