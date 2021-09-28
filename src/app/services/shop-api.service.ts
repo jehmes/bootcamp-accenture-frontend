0a2f4646c740ab7913826d54598837c6b6fd17dd
jehmes <jeihmesthales@hotmail.com>
2021-09-28 01:22:52 -0300

    Criado api do produto

---
+++ src/app/services/shop-api.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShopApiService {
+
+  urlGetAll = "http://localhost:8080/product"
+  urlGetById = "http://localhost:8080/product/"
+  urlCreate = "http://localhost:8080/product"
+  urlDelete = "http://localhost:8080/product/delete/"
+  urlUpdate = "http://localhost:8080/product/update/"
+
+  constructor(private http: HttpClient) { }
+  
+  createProduct(product: any): Observable<any> {
+    return this.http.post<any>(this.urlCreate, product)
+  }
+
+  getAllProducts(): Observable<any> {
+    return this.http.get<any>(this.urlGetAll)
+  }
+
+  getProductById(id: number): Observable<any> {
+    return this.http.get<any>(`${this.urlGetById}${id}`)
+  }
+
+  deleteProduct(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.urlDelete}${id}`)
+  }
+
+  updateUser(product: any, id: number): Observable<any> {
+    return this.http.post<any>(`${this.urlUpdate}${id}`, product)
+  }
+}
